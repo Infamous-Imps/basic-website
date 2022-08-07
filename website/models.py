69cf60c1900fa9eb9fbb19b1de4747fcc8012a4f
SigireddyBalasai <101649913+SigireddyBalasai@users.noreply.github.com>
2022-08-07 11:32:45 +0530

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,8 +1,6 @@
 from .db import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-class User(db):
-    def
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
